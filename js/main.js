2e079483f4667a5ab52420e3be888bbe1f5ef96c
DQ Gyumin Choi <choigyumin@gmail.com>
2022-04-24 21:46:54 +0900

    fix: rotate period issue... change default value

--- js/main.js
+++ js/main.js
@@ -293,7 +293,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 1000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
